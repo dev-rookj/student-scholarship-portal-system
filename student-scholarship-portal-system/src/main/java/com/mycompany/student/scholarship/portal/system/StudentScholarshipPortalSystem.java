a13f8ba54b2c330cc77ccae894ea11f89a562828
dev-rookj <jeffersonmichaelriosa07@gmail.com>
2024-12-26 19:50:28 +0800

    initial commit

---
+++ student-scholarship-portal-system/src/main/java/com/mycompany/student/scholarship/portal/system/StudentScholarshipPortalSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.student.scholarship.portal.system;
+
+/**
+ *
+ * @author Windows
+ */
+public class StudentScholarshipPortalSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
